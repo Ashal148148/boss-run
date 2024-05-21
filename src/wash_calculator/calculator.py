dac16c5b92cab313edf007bfa7f7e8445c4e8788
Ashal <shaulca123@gmail.com>
2024-05-21 10:30:31 +0300

    lvl focused washing

--- src/wash_calculator/calculator.py
+++ src/wash_calculator/calculator.py
@@ -3,7 +3,7 @@ from src.wash_calculator.player import Player
 from src.wash_calculator.equipment import Equipment
 from src.wash_calculator.job import jobs
 
-def do_the_stuff(player: Player, int_gears: List[Equipment], level_goal: int, hp_goal: int) -> Tuple[int,int, int, int, bool]:
+def do_the_stuff(player: Player, int_gears: List[Equipment], level_goal: int, hp_goal: int) -> Tuple[int, int, int, int, bool]:
     player = player.copy()
     base_int = 10
     base_int_increment = 10
@@ -62,7 +62,29 @@ def do_the_stuff(player: Player, int_gears: List[Equipment], level_goal: int, hp
     print(f"[{player.name}] i have found the best base int: {min_base_int} and it is accompanied by {min_mp_washes} points into MP and {min_total_washes - min_mp_washes} Washes")
     return min_base_int, min_mp_washes, best_health, player.washes, success, player.fresh_ap_into_hp_total, (min_total_washes - min_mp_washes)
 
-# TODO mage HP wash
+
+def reach_the_goal_no_matter_what(player: Player, int_gears: List[Equipment], level_goal: int, hp_goal: int):
+    base_int, points_into_mp, best_health, washes, success, fresh_ap_into_hp_total, points_taken_out_of_mp = do_the_stuff(player, int_gears, level_goal, hp_goal)
+    if success:
+        return base_int, points_into_mp, best_health, washes, success, fresh_ap_into_hp_total, points_taken_out_of_mp, base_int
+    else:
+        missing_health = hp_goal - best_health
+        missing_mp = int(missing_health / player.job.base_hp_gain) * player.job.mp_cost
+        mp_wash_starting_int = base_int
+        generated_mp = 0
+        while mp_wash_starting_int > (50 + fresh_ap_into_hp_total) and generated_mp < missing_mp:
+            mp_wash_starting_int -= 1
+            points_into_mp += 1
+            washes += 1
+            points_taken_out_of_mp += 1
+            generated_mp += (mp_wash_starting_int / 10 - 2)  # minus 2 because of the 2 mp tax per fresh ap
+        washes += int(generated_mp / player.job.mp_cost)
+        best_health += int(generated_mp / player.job.mp_cost) * player.job.base_hp_gain
+        success = generated_mp >= missing_mp
+        print(f'missing mp {missing_mp} missing hp {missing_health} generated_mp {generated_mp} generated hp {int(generated_mp / player.job.mp_cost) * player.job.base_hp_gain}')
+        return base_int, points_into_mp, best_health, washes, success, fresh_ap_into_hp_total, points_taken_out_of_mp, mp_wash_starting_int
+
+
 def mage_mp_wash_planner(player: Player, int_gears: List[Equipment], level_goal: int, mp_goal: int):
     start_level = 20
     level_increment = 1
@@ -97,9 +119,26 @@ def mage_mp_wash_planner(player: Player, int_gears: List[Equipment], level_goal:
             ideal_mp_washes = player.mp_washes
             ideal_start_level = start_level
             ideal_total_mana = player.mana
-        print(f'[{player.name}] starting at lvl {start_level} has {sub_success} washes: {player.mp_washes} total mana: {player.mana}') 
+        # print(f'[{player.name}] starting at lvl {start_level} has {sub_success} washes: {player.mp_washes} total mana: {player.mana}') 
         start_level += level_increment
     print(f'[{player.name}] final results are {success} starting level: {ideal_start_level} washes: {ideal_mp_washes} total mana: {ideal_total_mana}')
+    return ideal_start_level, ideal_mp_washes, ideal_total_mana, success
+
+
+def mage_hp_wash_planner(player: Player, int_gears: List[Equipment], level_goal: int, hp_goal: int, mp_goal: int):
+    base_health_at_goal = player.job.base_hp[level_goal - 1]
+    bonus_mana_needed = int((hp_goal - base_health_at_goal) / 6) * 30
+    print(f"planning to hp wash mage will need: {bonus_mana_needed} bonus mana")
+    start_level, mp_washes, total_mana, is_successful = mage_mp_wash_planner(player, int_gears, level_goal, mp_goal + bonus_mana_needed)
+    if is_successful:
+        points_reset_from_mp_to_hp = int((hp_goal - base_health_at_goal) / 6)
+    else:
+        points_reset_from_mp_to_hp = int((total_mana - mp_goal) / 30)
+    print()
+    total_washes = points_reset_from_mp_to_hp + mp_washes
+    total_hp = base_health_at_goal + points_reset_from_mp_to_hp * 6
+    total_mana -= points_reset_from_mp_to_hp * 30
+    return start_level, total_washes, total_hp, total_mana, is_successful
 
 
 if __name__ == "__main__":
