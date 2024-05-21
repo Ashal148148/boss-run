38194d75e9b8ba66dc5f4e57f5862d11c312519c
Ashal <shaulca123@gmail.com>
2024-05-21 10:29:04 +0300

    added categories list and moved gears function here

--- src/wash_calculator/equipment.py
+++ src/wash_calculator/equipment.py
@@ -7,6 +7,29 @@ class Equipment:
     INT: int
     id: int
 
+    categories: list[str] = [
+        'Hat',
+        'Face Accessory',
+        'Eye Accessory',
+        'Earring',
+        'Overall',
+        'Top',
+        'Bottom',
+        'Shoes',
+        'Glove',
+        'Weapon',
+        'Cape',
+        'Pendant',
+        'Shield',
+        'Ring 1',
+        'Ring 2',
+        'Ring 3',
+        'Ring 4',
+        'NX pendant',
+        'Extra 1',
+        'Extra 2'
+    ]
+
     def __init__(self, category: str, name: str, level: int, INT: int, id: int = None) -> None:
         self.category = category
         self.name = name
@@ -22,3 +45,21 @@ class Equipment:
     
     def __eq__(self, __value: object) -> bool:
         return self.__repr__() == __value.__repr__()
+    
+
+def battlecat_gears(int_gears: List[Equipment]):
+    int_gears.append(Equipment("Hat",'horns',22, 5))
+    int_gears.append(Equipment("Hat",'zak',50, 32))
+    int_gears.append(Equipment("Earring", 'ear',15, 8))
+    int_gears.append(Equipment("Weapon", 'wooden',10, 8))
+    int_gears.append(Equipment("Overall", 'bathrobe',20, 20))
+    int_gears.append(Equipment("Pendant", 'yellow muffler',30, 3))
+    int_gears.append(Equipment("Pendant", 'dep star',50, 5))
+    int_gears.append(Equipment("Pendant", 'htp',120, 22))
+    int_gears.append(Equipment("Shield", 'pan shield',10,7))
+    int_gears.append(Equipment("Eye accessory", 'raccoon',45, 11))
+    int_gears.append(Equipment("Cape", 'ragged cape', 32, 9))
+    int_gears.append(Equipment("Cape", 'yellow cape', 50, 12))
+    int_gears.append(Equipment("Cape", 'cwkpq cape',80, 18))
+    int_gears.append(Equipment("Shoes", 'slime shoe',30, 1))
+    int_gears.append(Equipment("Glove", 'red markers', 20, 11))
